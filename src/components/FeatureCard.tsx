dcb06fe135b32e8d219169aa6326416cdc0a074c
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-05-07 22:10:29 +0530

    add custom google-font

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -8,9 +8,11 @@ type FeatureCardProps = {
 
 const FeatureCard = ({ title, description, demo }: FeatureCardProps): JSX.Element => {
   return (
-    <div className="container bg-white p-6 rounded-2xl shadow-md border border-gray-200">
-    <h2 className="text-xl font-bold mb-4">{title}</h2>
-    <h3 className="text-xl font-bold mb-4">{description}</h3>
+    <div className="container font-primary font-bold bg-white p-6 rounded-2xl shadow-md border border-gray-200">
+    <h1 className="mb-4">{title}</h1>
+    <h2 className="font-bold mb-4">{title}</h2>
+    <h3 className="font-bold mb-4">{description}</h3>
+    <p className="mb-4">{description}</p>
     {demo}
     </div>
   )
