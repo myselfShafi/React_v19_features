35f04e836915d349ef3664ee826570ef59991785
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-05-12 22:04:53 +0530

    aqua-color palette, change svg icon

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import "./index.css";
 function App() {
 
   return (
-    <div className="bg-blue-50 min-h-screen w-full">
-      <div className="p-5">
-        <h1 className="font-extrabold text-center">React version.19</h1>
-        <div className="h-100 grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {
-          v19Features.map(feature => <FeatureCard title={feature.title} description={feature.desc} demo="sfsafasfsaf" />)
-        }
-        </div>
+    <div className="p-5 min-h-screen bg-aqua-100">
+      <h1 className="font-extrabold text-center">React version.19</h1>
+      <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {
+        v19Features.map(feature => <FeatureCard key={feature.title} title={feature.title} description={feature.desc} demo="sfsafasfsaf" />)
+      }
       </div>
     </div>
   )
