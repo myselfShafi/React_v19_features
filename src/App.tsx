d456647dd83bdca70f725a644ca881c49d4e71f1
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-05-31 12:11:54 +0530

    animation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Fragment, useState } from "react";
+import { motion } from "motion/react";
+import { useState } from "react";
 import { FeatureCard, ThemeButton } from "./components";
 import { v19Features } from "./configs/v19.config";
 import "./index.css";
@@ -9,19 +10,19 @@ function App() {
     <div className="relative sm:p-5 min-h-screen bg-aqua-200 dark:bg-aqua-700">
       <ThemeButton />
       <h1 className="text-center text-aqua-700 dark:text-aqua-100 tracking-wide">React version.19</h1>
-      <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <motion.div layout className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {
           v19Features.map((feature, index) =>
-            <Fragment key={feature.id}>
               <FeatureCard
+                key={feature.id}
+                cId={feature.id}
                 title={feature.title}
                 description={feature.desc}
                 onClick={() => setFeatIndex(prev => (prev === index ? null : index))}
                 isExpanded={featIndex === index && !!feature.element}
-              />
-            </Fragment>)
+              />)
         }
-      </div>
+      </motion.div>
     </div>
   )
 }
--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -3,33 +3,23 @@ import { type HTMLAttributes, type JSX } from 'react';
 import ContentCard from './ContentCard';
 
 type FeatureCardProps = HTMLAttributes<HTMLDivElement> & MotionProps & {
+    cId: number;
     title: string;
     description: string;
     demo?: string;
     isExpanded?: boolean;
 }
 
-const FeatureCard = ({ title, description, demo, isExpanded = false, ...rest }: FeatureCardProps): JSX.Element => {
+const FeatureCard = ({ cId, title, description, demo, isExpanded = false, ...rest }: FeatureCardProps): JSX.Element => {
   return (
-    // <div className={`w-full max-w-lg mx-auto text-aqua-700 dark:text-aqua-100 rounded-2xl shadow-aqua-300 dark:shadow-aqua-600 transition-all duration-300 ease-in-out p-5 space-y-5 hover:cursor-pointer ${isExpanded ? "shadow-xl col-span-full order-first" : "hover:shadow-xl"}`} {...rest}>
-    //   <div className="space-y-2">
-    //     <h3 className="uppercase tracking-wider">{title}</h3>
-    //     <p className='text-aqua-600 dark:text-aqua-200'>{description}</p>
-    //   </div>
-    //   {demo && (
-    //     <div className="bg-slate-100 border border-slate-200 rounded-xl p-4 text-sm text-slate-700">
-    //       {demo}
-    //     </div>
-    //   )}
-    // </div>
-       <motion.div 
-       layout
-       transition={{ layout: { duration: 1.0, type: 'spring' } }}
-       className={`w-full max-w-11/12 sm:max-w-4/5 mx-auto text-aqua-700 dark:text-aqua-100 rounded-2xl shadow-aqua-300 dark:shadow-aqua-600 transition-all duration-300 ease-in-out space-y-5 hover:cursor-pointer ${isExpanded ? "shadow-xl col-span-full order-first" : "hover:shadow-xl"}`} {...rest}>
-      <motion.div layout className="space-y-2">
-        <motion.h3 layout className="uppercase tracking-wider">{title}</motion.h3>
+       <motion.div
+       layoutId={`card-${cId}`}
+       className={`w-full p-5 mx-auto text-aqua-700 dark:text-aqua-100 rounded-2xl shadow-aqua-300 dark:shadow-aqua-600 transition-all duration-300 ease-in-out space-y-5 hover:cursor-pointer ${isExpanded ? "shadow-xl col-span-full order-first" : "hover:shadow-xl"}`} 
+       {...rest}>
+      <div className="space-y-2">
+        <h3 className="uppercase tracking-wider">{title}</h3>
         <p className='text-aqua-600 dark:text-aqua-200'>{description}</p>
-      </motion.div>
+      </div>
       {demo && (
         <div className="bg-slate-100 border border-slate-200 rounded-xl p-4 text-sm text-slate-700">
           {demo}
@@ -37,9 +27,9 @@ const FeatureCard = ({ title, description, demo, isExpanded = false, ...rest }: 
       )}
       <AnimatePresence>
       {isExpanded && ( 
-      <motion.div className="col-span-full">
+      <div className="col-span-full">
         <ContentCard content="aasdsa" />
-      </motion.div>
+      </div>
       )}
       </AnimatePresence>
     </motion.div>
